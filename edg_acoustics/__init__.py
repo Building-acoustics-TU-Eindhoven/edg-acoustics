d443a7d85d67008ccfa14463a039871cd71eff49
Huiqing Wang <hqwang815@gmail.com>
2022-11-16 15:05:29 +0100

    first commit

---
+++ edg_acoustics/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about edg_acoustics"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Huiqing Wang"
+__email__ = "h.wang6@tue.nl"
+__version__ = "0.1.0"
