a3846f3037156c922d6a3e94b2648c59284ace17
Artur Palha <artur.palha@gmail.com>
2023-08-21 14:35:39 +0200

    Generalizes mesh file to be usable by any user.

--- examples/ex_01.py
+++ examples/ex_01.py
@@ -1,16 +1,24 @@
+import os
+
 import edg_acoustics
 #from edg_acoustics.mesh import Mesh
 # print(dir(edg_acoustics))
 
+# Boundary conditions
 BC_labels =  {'slip': 11, 'impedance1': 13, 'impedance2': 14, 'impedance3': 15}
-filename = "/Users/apalha/work/dev/edg_acoustics/edg-acoustics/data/tests/mesh/CoarseMesh.msh"
-mesh = edg_acoustics.Mesh(filename, BC_labels)
-Nx = 3
-Nt = 3
+
+# Mesh
+mesh_name = 'CoarseMesh.msh'
+mesh_data_folder = os.path.abspath(os.path.join(os.path.split(os.path.abspath(__file__))[0], os.path.pardir, 'data', 'tests', 'mesh'))
+mesh_filename = os.path.join(mesh_data_folder, mesh_name)
+mesh = edg_acoustics.Mesh(mesh_filename, BC_labels)
+
+# Approximation degrees
+Nx = 3  # in space
+Nt = 3  # in time
 
 sim = edg_acoustics.AcousticsSimulation(Nx, Nt, mesh, BC_labels)
 sim.init_local_system()
 
 print('Finished!')
-
 
