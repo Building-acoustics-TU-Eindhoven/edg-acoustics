7cd50210eda8f0f48ac9f7470751d65ca9776484
Artur Palha <artur.palha@gmail.com>
2023-11-23 10:01:38 +0100

    Alternative proposal for InitialCondition - Adds abstract class InitialCondition - Adds derived class Monopole_IC - Enables more general construciton of initial conditions and more generic interface to them in the AcousticsSimulation class.

--- edg_acoustics/__init__.py
+++ edg_acoustics/__init__.py
@@ -3,7 +3,7 @@ import logging
 from .acoustics_simulation import AcousticsSimulation
 from .mesh import Mesh
 from .boundary_condition import BoundaryCondition
-from .initial_condition import InitialCondition
+from .initial_condition import InitialCondition, Monopole_IC
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
--- edg_acoustics/initial_condition.py
+++ edg_acoustics/initial_condition.py
@@ -14,6 +14,7 @@ Setup Initial Condition
 from __future__ import annotations
 import meshio
 import numpy
+import abc
 import edg_acoustics
 from edg_acoustics.acoustics_simulation import AcousticsSimulation
 
@@ -22,82 +23,130 @@ __all__ = ['InitialCondition','FREQ_MAX']
 # Constants
 FREQ_MAX = 2e3  # maximum resolvable frequency
 
-class InitialCondition(AcousticsSimulation):
-    """Setup initial condition of a DG acoustics simulation for a specific scenario.
-
-    :class:`.InitialCondition` is used to load the boundary condition parameters, determine the time step size.
-
-    Args:
-        xyz (numpy.ndarray): ``[3, Np, N_tets]``the physical space coordinates :math:`(x, y, z)` of the collocation points of each
-                element of the mesh. ``xyz[0]`` contains the x-coordinates, ``xyz[1]`` contains the y-coordinates,
-                 ``xyz[2]`` contains the z-coordinates.
-        source_xyz (numpy.ndarray): an (3, ) array containing the physical coordinates of the monopole source.
-        halfwidth (float): half-bandwidth of the initial Gaussian pulse.
-        Np (int): number of collocation nodes in an element.
-
-        freq_max (float): maximum resolvable frequency of the simulation. <default>: edg_acoustics.FREQ_MAX
-
-    # Raises:
-    #     ValueError: If BCpara.keys() is not present in the acoustics_simulation.BC_list.values(), an error is raised. 
-    #         If a label is present in the acoustics_simulation.BC_list.values() but not in BCpara, an error is raised.
-
-    Attributes:
-        P (numpy.ndarray): ``[Np, N_tets]`` the physical space coordinates :math:`(x, y, z)` of the collocation points of each element of the\
-            mesh. ``xyz[0]`` contains the x-coordinates, ``xyz[1]`` contains the y-coordinates, ``xyz[2]``
-            contains the z-coordinates.
 
 
+class InitialCondition(abc.ABC):
+    @abc.abstractmethod
+    def __init__(self):
+        pass
 
-    Example:
-        An element of this class can be initialized in the following way
+    @abc.abstractmethod
+    def P(self, xyz: numpy.ndarray)
+        pass
 
+    @abc.abstractmethod
+    def U(self, xyz: numpy.ndarray)
+        pass
 
-    """
-    #     self.IC = edg_acoustics.InitialCondition(self.mesh, source_xyz, halfwidth, self.Np)
-    #     self.IC.set_source_location(source_xyz)
-    #     self.IC.set_frequency(halfwidth)
-    #     self.initial_condition_field=self.IC.compute_field() #values at nodes, 
-    def __init__(self, P: numpy.ndarray, U: numpy.ndarray, V: numpy.ndarray, W: numpy.ndarray):
-        # Store input parameters
-        self.P = P
-        self.U = U
-        self.V = V
-        self.W = W
+    @abc.abstractmethod
+    def V(self, xyz: numpy.ndarray)
+        pass
 
-        # self.P = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-        # self.U = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-        # self.V = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-        # self.W = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-
-        # self.source_xyz = InitialCondition.set_source_location(source_xyz)
-        # self.halfwidth = halfwidth
-        # self.P,self.U, self.V, self.W = InitialCondition.monopole(xyz, source_xyz, halfwidth)
-
-    # Static methods ---------------------------------------------------------------------------------------------------
-    @staticmethod
-    def set_source_location(source_xyz: float):
-        return source_xyz
-    
-    @staticmethod
-    def set_frequency(halfwidth: float):
-        return halfwidth
-    
-    # @classmethod
-    # def monopole(cls, self, source_xyz: float, halfwidth: float):
-    #     P = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
-    #     U = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
-    #     V = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
-    #     W = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
+    @abc.abstractmethod
+    def W(self, xyz: numpy.ndarray)
+        pass
         
-    #     P = numpy.exp(-numpy.log(2) * ((self.xyz[0] - source_xyz[0])**2 + (self.xyz[1] - source_xyz[1])**2 + (self.xyz[2] - source_xyz[2])**2) / halfwidth**2)
-    #     return cls(P, U, V, W)
 
-    @classmethod
-    def monopole(cls, xyz: numpy.ndarray, source_xyz: float, halfwidth: float):
-        P = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-        U = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-        V = numpy.zeros([xyz.shape[1], xyz.shape[2]])
-        W = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+
+class Monopole_IC(InitialCondition):
+
+    def __init__(self, source_xyz: numpy.ndarray, halfwidth: float):
+        self.source_xyz = source_xyz
+        self.halfwidth = halfwidth
+
+    def P(self, xyz: numpy.ndarray):
+        pressure = numpy.exp(-numpy.log(2) * ((xyz[0] - self.source_xyz[0])**2 + (xyz[1] - self.source_xyz[1])**2 + (xyz[2] - self.source_xyz[2])**2) / self.halfwidth**2)
+        return pressure
+    
+    def U(self, xyz: numpy.ndarray):
+        return numpy.zeros([xyz.shape[1], xyz.shape[2]])
+    
+    def V(self, xyz: numpy.ndarray):
+        return numpy.zeros([xyz.shape[1], xyz.shape[2]])
+    
+    def W(self, xyz: numpy.ndarray):
+        return numpy.zeros([xyz.shape[1], xyz.shape[2]])
+
+
+
+
+
+# class InitialCondition(AcousticsSimulation):
+#     """Setup initial condition of a DG acoustics simulation for a specific scenario.
+
+#     :class:`.InitialCondition` is used to load the boundary condition parameters, determine the time step size.
+
+#     Args:
+#         xyz (numpy.ndarray): ``[3, Np, N_tets]``the physical space coordinates :math:`(x, y, z)` of the collocation points of each
+#                 element of the mesh. ``xyz[0]`` contains the x-coordinates, ``xyz[1]`` contains the y-coordinates,
+#                  ``xyz[2]`` contains the z-coordinates.
+#         source_xyz (numpy.ndarray): an (3, ) array containing the physical coordinates of the monopole source.
+#         halfwidth (float): half-bandwidth of the initial Gaussian pulse.
+#         Np (int): number of collocation nodes in an element.
+
+#         freq_max (float): maximum resolvable frequency of the simulation. <default>: edg_acoustics.FREQ_MAX
+
+#     # Raises:
+#     #     ValueError: If BCpara.keys() is not present in the acoustics_simulation.BC_list.values(), an error is raised. 
+#     #         If a label is present in the acoustics_simulation.BC_list.values() but not in BCpara, an error is raised.
+
+#     Attributes:
+#         P (numpy.ndarray): ``[Np, N_tets]`` the physical space coordinates :math:`(x, y, z)` of the collocation points of each element of the\
+#             mesh. ``xyz[0]`` contains the x-coordinates, ``xyz[1]`` contains the y-coordinates, ``xyz[2]``
+#             contains the z-coordinates.
+
+
+
+#     Example:
+#         An element of this class can be initialized in the following way
+
+
+#     """
+#     #     self.IC = edg_acoustics.InitialCondition(self.mesh, source_xyz, halfwidth, self.Np)
+#     #     self.IC.set_source_location(source_xyz)
+#     #     self.IC.set_frequency(halfwidth)
+#     #     self.initial_condition_field=self.IC.compute_field() #values at nodes, 
+#     def __init__(self, P: numpy.ndarray, U: numpy.ndarray, V: numpy.ndarray, W: numpy.ndarray):
+#         # Store input parameters
+#         self.P = P
+#         self.U = U
+#         self.V = V
+#         self.W = W
+
+#         # self.P = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+#         # self.U = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+#         # self.V = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+#         # self.W = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+
+#         # self.source_xyz = InitialCondition.set_source_location(source_xyz)
+#         # self.halfwidth = halfwidth
+#         # self.P,self.U, self.V, self.W = InitialCondition.monopole(xyz, source_xyz, halfwidth)
+
+#     # Static methods ---------------------------------------------------------------------------------------------------
+#     @staticmethod
+#     def set_source_location(source_xyz: float):
+#         return source_xyz
+    
+#     @staticmethod
+#     def set_frequency(halfwidth: float):
+#         return halfwidth
+    
+#     # @classmethod
+#     # def monopole(cls, self, source_xyz: float, halfwidth: float):
+#     #     P = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
+#     #     U = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
+#     #     V = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
+#     #     W = numpy.zeros([self.xyz.shape[1], self.xyz.shape[2]])
         
-        P = numpy.exp(-numpy.log(2) * ((xyz[0] - source_xyz[0])**2 + (xyz[1] - source_xyz[1])**2 + (xyz[2] - source_xyz[2])**2) / halfwidth**2)
-        return cls(P, U, V, W)
+#     #     P = numpy.exp(-numpy.log(2) * ((self.xyz[0] - source_xyz[0])**2 + (self.xyz[1] - source_xyz[1])**2 + (self.xyz[2] - source_xyz[2])**2) / halfwidth**2)
+#     #     return cls(P, U, V, W)
+
+#     @classmethod
+#     def monopole(cls, xyz: numpy.ndarray, source_xyz: float, halfwidth: float):
+#         P = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+#         U = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+#         V = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+#         W = numpy.zeros([xyz.shape[1], xyz.shape[2]])
+        
+#         P = numpy.exp(-numpy.log(2) * ((xyz[0] - source_xyz[0])**2 + (xyz[1] - source_xyz[1])**2 + (xyz[2] - source_xyz[2])**2) / halfwidth**2)
+#         return cls(P, U, V, W)
