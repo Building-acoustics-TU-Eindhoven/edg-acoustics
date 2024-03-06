57211016e1007aba000375903cc49fec1924f238
Huiqing Wang <hqwang815@gmail.com>
2024-03-06 14:15:19 +0100

    Refactor time integration class in edg_acoustics module

--- edg_acoustics/time_integration.py
+++ edg_acoustics/time_integration.py
@@ -1,6 +1,6 @@
 """
 ``edg_acoustics.time_integration``
-======================
+==================================
 
 The edg_acoustics time_integration ***** UPDATE ****  provide more necessary functionalities 
 (based upon :mod:`edg_acoustics.acoustics_simulation`) 
@@ -9,7 +9,7 @@ to setup initial condition for a specific scenario.
 Functions and classes present in :mod:`edg_acoustics.initial_condition` are listed below.
 
 Setup Initial Condition
----------------
+-----------------------
     InitialCondition
 """
 
@@ -17,7 +17,6 @@ from __future__ import annotations
 
 import typing
 import abc
-import math
 import numpy
 import edg_acoustics
 
@@ -54,7 +53,21 @@ class TimeIntegrator(abc.ABC):
 
 class TSI_TI(TimeIntegrator):
     """
-    class for time integrator of Taylor-series.
+    class for time integrator of Taylor-series time integration scheme.
+
+    :class:`.TSI_TI` is used to evolve the pressure and velocity at the time T to time T + dt, based on the Taylor-series time integration scheme.
+
+    Args:
+    L_operator (typing.Callable[[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, list]]): the function in AcousticSimulation that enables the computation of Lq, given q = [P, Vx, Vy, Vz]
+    dtscale (float): the time step scale
+    Nt (int): the order of the time integration scheme.
+    CFL (float): the CFL number
+
+    Attributes:
+    L_operator (typing.Callable[[numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray, list]]): the function in AcousticSimulation that enables the computation of Lq, given q = [P, Vx, Vy, Vz]
+    Nt (int): the order of the time integration scheme.
+    CFL (float): the CFL number
+    dt (float): the time step
     """
 
     def __init__(
@@ -64,10 +77,9 @@ class TSI_TI(TimeIntegrator):
         Nt: int,
         CFL: float = CFL_Default,
     ):
-        # Nt (int): the order of the time integration scheme.
 
-        self.L_operator = L_operator  # the function in AcousticSimulation that enables the computation of Lq, given q = [P, Vx, Vy, Vz]
-        self.Nt = Nt  # degree of time integration
+        self.L_operator = L_operator
+        self.Nt = Nt
         self.CFL = CFL
         self.dt = CFL * dtscale
 
@@ -92,7 +104,6 @@ class TSI_TI(TimeIntegrator):
         Vx0 = Vx.copy()
         Vy0 = Vy.copy()
         Vz0 = Vz.copy()
-        print(f"inside, P0 ID {id(P0)}, P ID {id(P)}")  # used for debugging
 
         for index, paras in enumerate(BC.BCpara):
             for polekey in paras:
@@ -102,42 +113,13 @@ class TSI_TI(TimeIntegrator):
                     BC.BCvar[index]["kexi1"] = BC.BCvar[index]["KEXI1"].copy()
                     BC.BCvar[index]["kexi2"] = BC.BCvar[index]["KEXI2"].copy()
 
-        ##########################
-        # for Tind in range(1, self.Nt + 1):
-        #     # Compute L (L^{Tind-1} q)
-        #     P0, Vx0, Vy0, Vz0, BC.BCvar = self.L_operator(P0, Vx0, Vy0, Vz0, BC.BCvar)
-
-        #     # Add the Taylor term \frac{dt^{Tind}}{Tind!}L^{Tind}q
-        #     Vx += self.dt**Tind / math.factorial(Tind) * Vx0
-        #     Vy += self.dt**Tind / math.factorial(Tind) * Vy0
-        #     Vz += self.dt**Tind / math.factorial(Tind) * Vz0
-        #     P += self.dt**Tind / math.factorial(Tind) * P0
-
-        #     for index, paras in enumerate(BC.BCpara):
-        #         for polekey in paras:
-        #             if polekey == "RP":
-        #                 BC.BCvar[index]["PHI"] += (
-        #                     self.dt**Tind / math.factorial(Tind) * BC.BCvar[index]["phi"]
-        #                 )
-        #             elif polekey == "CP":
-        #                 BC.BCvar[index]["KEXI1"] += (
-        #                     self.dt**Tind / math.factorial(Tind) * BC.BCvar[index]["kexi1"]
-        #                 )
-        #                 BC.BCvar[index]["KEXI2"] += (
-        #                     self.dt**Tind / math.factorial(Tind) * BC.BCvar[index]["kexi2"]
-        #                 )
-
         for Tind in range(1, self.Nt + 1):
-            # Compute L (L^{Tind-1} q)
             P0, Vx0, Vy0, Vz0, BC.BCvar = self.L_operator(P0, Vx0, Vy0, Vz0, BC.BCvar)
-
-            print(f"check before, P0 ID {id(P0)}")  # used for debugging
 
             P0 *= self.dt / Tind
             Vx0 *= self.dt / Tind
             Vy0 *= self.dt / Tind
             Vz0 *= self.dt / Tind
-            print(f"check after, P0 ID {id(P0)}")  # used for debugging
 
             P += P0
             Vx += Vx0
