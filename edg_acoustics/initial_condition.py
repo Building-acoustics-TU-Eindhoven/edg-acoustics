55274dbc84d149bd833272e9ca04899ed74b77f6
Huiqing Wang <hqwang815@gmail.com>
2024-06-28 14:37:11 +0200

    feat: Add functionality to automatically prescribes the halfwidth based on user input interested frequency value.
    The commit adds a new solve_halfwidth method to the Monopole_IC class in the edg_acoustics.initial_condition module. This method solves for the halfwidth of the initial Gaussian pulse, given a frequency, using linear interpolation. It improves the accuracy of the initial condition setup for pressure in the simulation.

--- edg_acoustics/initial_condition.py
+++ edg_acoustics/initial_condition.py
@@ -6,6 +6,7 @@ The current version of edg_acoustics.initial_condition provides monopole source 
 from __future__ import annotations
 import abc
 import numpy
+from scipy.interpolate import interp1d
 
 __all__ = ["InitialCondition", "Monopole_IC"]
 
@@ -52,9 +53,27 @@ class Monopole_IC(InitialCondition):
         halfwidth (float): half-bandwidth of the initial Gaussian pulse.
     """
 
-    def __init__(self, source_xyz: numpy.ndarray, halfwidth: float):
+    def __init__(self, source_xyz: numpy.ndarray, frequency: float):
         self.source_xyz = source_xyz
-        self.halfwidth = halfwidth
+        self.halfwidth = Monopole_IC.solve_halfwidth(frequency)
+
+    @staticmethod
+    def solve_halfwidth(frequency: float):
+        """Solve halfwidth of the initial Gaussian pulse, given a frequency, using linear interpolation. Avoids root choosing issue with analytical spectra of Gaussian pulse.
+
+        Args:
+            frequency (float): frequency of the monopole source.
+
+        Returns:
+            halfwidth (float): halfwidth of the initial Gaussian pulse.
+        """
+        # Given data points
+        x_points = numpy.array([300, 500, 800, 1000, 2000, 4000])
+        y_points = numpy.array([0.4, 0.33, 0.23, 0.17, 0.09, 0.05])
+
+        linear_interp = interp1d(x_points, y_points, kind="linear")
+
+        return linear_interp(frequency)
 
     def Pinit(self, xyz: numpy.ndarray):
         """Setup initial condition for pressure."""
